5acc0dc9dc3f0b3efbfa73e1ac7a8b1f72b78635
David Crandell <guitardave@users.noreply.github.com>
2018-08-31 10:50:26 -0500

    Add project files.

---
+++ VoIPFileConverter/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace VoIPFileConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CCITT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(205, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MP3";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Input File";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(204, 50);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Open";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 107);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(266, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Output File";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 171);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "VoIP Output File Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ VoIPFileConverter/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Alvas.Audio;
+
+namespace VoIPFileConverter
+{
+    public partial class Form1 : Form
+    {
+        public const string path = @"\\\\192.168.3.241\\ManualMix\\Good\\";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            
+
+            // OPEN FILE
+            using (openFileDialog1)
+            {
+                openFileDialog1.InitialDirectory = path;
+                openFileDialog1.FilterIndex = 2;
+                openFileDialog1.RestoreDirectory = true;
+            }
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // DO STUFF
+                textBox1.Text = openFileDialog1.FileName.ToString();
+            }
+            else
+            {
+                MessageBox.Show("There was a problem opening the file explorer");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // CONVERT TO CCITT
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ConvertToMP3("", "", 0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openFileDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+        private void ConvertToCCITT(String f, String FileName)
+        {
+            try
+            {
+
+                WaveReader newWr = new WaveReader(File.OpenRead(f));
+                IntPtr oldPcm = newWr.ReadFormat();
+                byte[] oldPcmData = newWr.ReadData();
+                IntPtr newFormat = AudioCompressionManager.GetPcmFormat(1, 16, 8000);
+                byte[] newData = { };
+
+                WaveFormat wf = AudioCompressionManager.GetWaveFormat(oldPcm);
+
+                newWr.Close();
+
+                if (FileName != "0")
+                    f = FileName;
+
+                if (File.Exists(textBox1.Text + Path.GetFileName(f)))
+                    File.Delete(textBox1.Text + Path.GetFileName(f));
+
+                // **** debug ****
+                //txtStatus.Text += f + eol;
+
+                int samp = wf.nSamplesPerSec;
+                int bps = wf.wBitsPerSample;
+
+                // sample rate is > 8000
+                if (samp > 8000)
+                {
+                    newData = AudioCompressionManager.Resample(oldPcm, oldPcmData, newFormat);
+                }
+
+                IntPtr ccittOut = AudioCompressionManager.GetCompatibleFormat(newFormat, AudioCompressionManager.MuLawFormatTag);
+                byte[] finalData = AudioCompressionManager.Convert(newFormat, ccittOut, newData, false);
+                WaveWriter finalWr = new WaveWriter(File.Create(textBox1.Text + Path.GetFileName(f)),
+                    AudioCompressionManager.FormatBytes(ccittOut));
+
+                finalWr.WriteData(finalData);
+                finalWr.Close();
+
+                // **** debug *****
+                //TextOps(dirUlaw + Path.GetFileName(f) + eol2);
+
+                //DeleteFile(f);
+
+            }
+            catch (NullReferenceException nex)
+            {
+                //TextOps("NullReferenceException: " + nex.Message.ToString() + cr);
+
+            }
+            catch (IOException iex)
+            {
+                //TextOps("IOException: " + iex.Message.ToString() + cr);
+
+            }
+            catch (AudioException ex)
+            {
+                //TextOps("AudioException: " + ex.Message.ToString() + cr);
+
+            }
+        }
+
+        // convert to MP3
+        private void ConvertToMP3(String f, String FileName, int LocType)
+        {
+            try
+            {
+
+                WaveReader wr = new WaveReader(File.OpenRead(f));
+                IntPtr oldFormat = wr.ReadFormat();
+                byte[] oldData = wr.ReadData();
+                wr.Close();
+
+                if (FileName != "0")
+                    f = FileName;
+
+                // if voip, write to voip dir otherwise write to MP3 (N7 & iLink) dir
+                if (LocType == 2)
+                    textBox1.Text = textBox2.Text;
+                else
+                    textBox1.Text = textBox1.Text;
+
+                if (File.Exists(textBox1.Text + Path.GetFileName(f)))
+                    File.Delete(textBox1.Text + Path.GetFileName(f));
+                //txtStatus.Text += f + eol;
+
+                IntPtr pcmFormat = AudioCompressionManager.GetCompatibleFormat(oldFormat,
+                    AudioCompressionManager.PcmFormatTag);
+                byte[] pcmData = AudioCompressionManager.Convert(oldFormat, pcmFormat, oldData, false);
+
+                IntPtr mp3Format = AudioCompressionManager.GetMp3Format(1, 112, 44100);
+
+                f = f.Replace("wav", "mp3");
+
+                Mp3Writer mw = new Mp3Writer(File.Create(textBox1.Text + Path.GetFileName(f)));
+
+                byte[] newData = AudioCompressionManager.Convert(pcmFormat, mp3Format, oldData, false);
+                mw.WriteData(newData);
+                mw.Close();
+
+                // **** debug *****
+                //TextOps(dirMp3 + Path.GetFileName(f) + eol2);
+
+                //DeleteFile(f);
+            }
+            catch (NullReferenceException nex)
+            {
+                //TextOps("NullReferenceException: " + nex.Message.ToString() + cr);
+
+            }
+            catch (IOException iex)
+            {
+                //TextOps("IOException: " + iex.Message.ToString() + cr);
+
+            }
+            catch (AudioException ex)
+            {
+                //TextOps("AudioException: " + ex.Message.ToString() + cr);
+
+            }
+        }
+
+        // CONVERT TO PCM 8KHz 16-bit mono
+        private void ConvertToPcm(String f, String FileName)
+        {
+            try
+            {
+                WaveReader wr = new WaveReader(File.OpenRead(f));
+                IntPtr oldFormat = wr.ReadFormat();
+                byte[] oldData = wr.ReadData();
+                IntPtr newFormat = AudioCompressionManager.GetPcmFormat(1, 16, 8000);
+                byte[] newData = { };
+                WaveFormat wf = AudioCompressionManager.GetWaveFormat(oldFormat);
+
+                wr.Close();
+
+                if (FileName != "0")
+                    f = FileName;
+
+                if (File.Exists(textBox1.Text + Path.GetFileName(f)))
+                    File.Delete(textBox1.Text + Path.GetFileName(f));
+
+                // **** debug ****
+                //txtStatus.Text += f + eol;
+
+                int samp = wf.nSamplesPerSec;
+                int bps = wf.wBitsPerSample;
+
+                // sample rate is > 8000
+                if (samp > 8000)
+                {
+                    newData = AudioCompressionManager.Resample(oldFormat, oldData, newFormat);
+                    WaveWriter ww = new WaveWriter(File.Create(textBox1.Text + Path.GetFileName(f)),
+                        AudioCompressionManager.FormatBytes(newFormat));
+                    ww.WriteData(newData);
+                    ww.Close();
+
+                    // **** debug *****
+                    //TextOps(dirPcm + Path.GetFileName(f) + eol2);
+                }
+                else
+                {
+                    //TextOps(Path.GetFileName(f) + " already exists at 8KHz" + cr);
+                }
+
+                //DeleteFile(f);
+
+            }
+            catch (NullReferenceException nex)
+            {
+                //TextOps("NullReferenceException: " + nex.Message.ToString() + cr);
+
+            }
+            catch (IOException iex)
+            {
+                //TextOps("IOException: " + iex.Message.ToString() + cr);
+
+            }
+            catch (AudioException ex)
+            {
+                //TextOps("AudioException: " + ex.Message.ToString() + cr);
+
+            }
+        }
+
+    }
+}
